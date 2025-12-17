b325b3a107cf7fcab0c068f335101150ada12926
Atia Amin <52665195+atiaamin2019@users.noreply.github.com>
2025-12-17 15:48:25 -0500

    Update extract_telomeric_reads.sh

--- scripts/05_telomere_extension/extract_telomeric_reads.sh
+++ scripts/05_telomere_extension/extract_telomeric_reads.sh
@@ -3,8 +3,10 @@
 minimap2 -a linear_plasmid_contigs.fasta bb_ont_filtered.fastq > ONT_mapped_to_linear_plasmid.sam
 
 # Convert to sorted BAM and index
-samtools view -bS ONT_mapped_to_linear_plasmid.sam | samtools sort -o ONT_mapped_to_linear_plasmid.sorted.bam
-samtools index ONT_mapped_to_linear_plasmid.sorted.bam
+samtools view -bS ONT_mapped_to_linear_plasmid.sam \
+| samtools view -h -F 0x900 - \
+| samtools sort -o ONT_mapped_to_linear_plasmid.sorted.bam - \
+&& samtools index ONT_mapped_to_linear_plasmid.sorted.bam
 
 # Filter reads based on BED regions
 samtools view -b ONT_mapped_to_linear_plasmid.sorted.bam | bedtools intersect -a - -b regions.bed > ends_mapped_reads.bam
